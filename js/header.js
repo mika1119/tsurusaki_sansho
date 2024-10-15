9cba228f9ebb6b6a0a10ef4156edf9692b9c65bb
mika1119 <mika1119.work@gmail.com>
2024-10-15 13:12:26 +0900

    ナビゲーション作成（モバイル）

--- js/header.js
+++ js/header.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   const btn = document.getElementById("headerBtn");
   const lines = document.getElementsByClassName("header-btn__line");
+  const nav = document.getElementById("nav");
 
   btn.addEventListener("click", function() {
     for (let i = 0; i < lines.length; i++) {
       lines[i].classList.toggle("open");
     }
+    nav.classList.toggle("open");
   })
 });
