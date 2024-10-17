3d3b4349ceebebb3c49e77db5d95fe2761f65039
mika1119 <mika1119.work@gmail.com>
2024-10-17 11:25:15 +0900

    swiper導入＆メインビジュアル作成

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const mainSwiper = new Swiper(".mv", {
+    loop: true,
+    speed: 4000,
+    effect: "fade",
+    autoplay: {
+      delay: 2000,
+    },
+  });
+})
