588f63a74daa7184a06a8ce847c08f1ab001a36c
mika1119 <mika1119.work@gmail.com>
2024-10-18 10:29:58 +0900

    保有車両（ギャラリー）作成

--- js/swiper.js
+++ js/swiper.js
@@ -7,4 +7,22 @@ document.addEventListener("DOMContentLoaded", function() {
       delay: 2000,
     },
   });
+
+  const gallerySwiper = new Swiper(".gallery", {
+    loop: true,
+    slidesPerView: 2,
+    speed: 10000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 0,
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 4,
+      },
+    },
+  });
 })
